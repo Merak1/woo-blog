01515744aa9543a48fa863091070ed1c3b52602e
root <root@DESKTOP-DH9FK14.localdomain>
2024-01-29 07:31:04 -0600

    modified prop types

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import wooLogo from "../../../public/woo-logosvg.svg";
+import SearchBar from "../search/SearchBar";
 
 const NavBar = () => {
   return (
     <div
-      className="w-full bg-teal-600 flex justify-evenly 
+      className="w-full bg-teal-600 flex justify-between 
       py-7 text-center
     "
     >
       <div className="w-1/6">
         <Image priority src={wooLogo} alt="Woo logo" />
       </div>
-      <div className="w-5/6 ">{/* <p>Small Internet Blog</p> */}</div>
+      <div className="w-5/6 ">
+        <SearchBar />
+      </div>
     </div>
   );
 };
---
+++ app/types/entryType.ts
@@ -0,0 +1,7 @@
+export interface EntryProps {
+  id: string;
+  user: string;
+  title: string;
+  content: string;
+  createdAt: string;
+}
