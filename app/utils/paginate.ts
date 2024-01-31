7b8624f435721fb919cee2f1ead9d9993f596a4f
root <root@DESKTOP-DH9FK14.localdomain>
2024-01-31 12:16:51 -0600

    added safegard to paginate when no items

--- app/utils/paginate.ts
+++ app/utils/paginate.ts
@@ -1,5 +1,8 @@
 export const Paginate = (data: any, currentPage: number, pageSize: number) => {
   const startIndex = (currentPage - 1) * pageSize;
 
+  if (data.status === 500) {
+    return null;
+  }
   return data.slice(startIndex, startIndex + pageSize);
 };
