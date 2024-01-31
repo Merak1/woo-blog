69de4860b58472c561f3fa64ebba2bb8b814602a
root <root@DESKTOP-DH9FK14.localdomain>
2024-01-31 16:14:07 -0600

    made case insensitive the search

--- app/utils/Search.ts
+++ app/utils/Search.ts
@@ -1,13 +1,13 @@
 export const Search = (data: any, searchQuery: string, searchBy: string) => {
   const searchedData = data.filter((element: any) => {
     if (searchBy === "title") {
-      return element.title.includes(searchQuery);
+      return element.title.toLowerCase().includes(searchQuery.toLowerCase());
     }
     if (searchBy === "user") {
-      return element.user.includes(searchQuery);
+      return element.user.toLowerCase().includes(searchQuery.toLowerCase());
     }
     if (searchBy === "content") {
-      return element.content.includes(searchQuery);
+      return element.content.toLowerCase().includes(searchQuery.toLowerCase());
     }
   });
   return searchedData;
